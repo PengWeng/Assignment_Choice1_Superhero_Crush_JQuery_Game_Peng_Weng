778b313f5b7dec031e662a4b6085ed22112d3c3c
Graphic <graphic@SOD-SJH-516-32M.local>
2015-11-27 14:11:02 -0500

    4th: V0.04 Basic JQuery

--- js/main.js
+++ js/main.js
@@ -1,1 +1,56 @@
+$(document).ready(function(){
 
+
+// 	$(".boxbatman").click(function(){
+// 		$(".boxbatman").removeClass("boxbatman");
+// 	});
+
+// 	$(".boxironman").click(function(){
+// 		$(".boxironman").removeClass("boxironman");
+// 	});
+
+
+// $(".boxcaptainamerica").click(function(){
+// 		$(".boxcaptainamerica").removeClass("boxcaptainamerica");
+// 	});
+
+
+
+var boxes = [
+	[1,2,3],
+	[4,5,6],
+	[7,8,9]
+];
+
+if {
+	boxes[1][1]==boxes[2][1]
+}
+
+
+
+
+for (int r=0; r < boxeslength; r++) {
+	boxes[r]
+
+	for (int c=0; c<boxeslength;c++) {
+		boxes[r][c]==boxes[r][c+1]
+
+
+	}
+
+	
+
+.
+}
+
+
+
+
+
+
+
+
+
+
+
+});
